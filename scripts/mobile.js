49028940c328b64752986c9c55c4fc6afdd0e2d9
Argett <lilian.favre.garcia@efrei.net>
2021-05-18 18:06:36 +0200

    Forgot rename

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -1,12 +1,12 @@
 /* global AFRAME */
-AFRAME.registerComponent('interractions', {
+AFRAME.registerComponent('mobile', {
   init: function () {
     if (AFRAME.utils.device.isMobile()) {
       this.setAttribute('visible', 'true')
-      console.log("MOBILE")
+      console.log('MOBILE')
     }else {
       this.setAttribute('visible', 'false')
-      console.log("PC")
+      console.log('PC')
     }
   }
 })
