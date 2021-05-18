29d11ce8abcd3e88137839bd84d50ff829d9c72e
Argett <lilian.favre.garcia@efrei.net>
2021-05-18 18:17:47 +0200

    Corrected variable mobile

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -1,16 +1,13 @@
 /* global AFRAME */
 AFRAME.registerComponent('mobile', {
   init: function () {
-    var tpsMain = document.querySelectorAll('#tp_circles_main')
-    var tpsSecond = document.querySelectorAll('#tp_circles_second')
-
     if (AFRAME.utils.device.isMobile()) {
-      tpsMain.setAttribute('visible', 'true')
-      tpsSecond.setAttribute('visible', 'true')
+        document.querySelector('#tp_circles_main').setAttribute('visible', true)
+        document.querySelector('#tp_circles_second').setAttribute('visible', true)
       console.log('MOBILE')
     }else {
-      tpsMain.setAttribute('visible', 'false')
-      tpsSecond.setAttribute('visible', 'true')
+        document.querySelector('#tp_circles_main').setAttribute('visible', false)
+        document.querySelector('#tp_circles_second').setAttribute('visible', false)
       console.log('PC')
     }
   }
