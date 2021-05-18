0717abe5ecd7841921767b4c8a6f3224463958dc
Argett <lilian.favre.garcia@efrei.net>
2021-05-18 18:06:00 +0200

    Debug text

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -3,8 +3,10 @@ AFRAME.registerComponent('interractions', {
   init: function () {
     if (AFRAME.utils.device.isMobile()) {
       this.setAttribute('visible', 'true')
+      console.log("MOBILE")
     }else {
       this.setAttribute('visible', 'false')
+      console.log("PC")
     }
   }
 })
