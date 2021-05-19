dbea8e709b5b900e9d71d000828c48dba686675e
Argett <lilian.favre.garcia@efrei.net>
2021-05-19 15:26:38 +0200

    Corrected positions

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -2,16 +2,14 @@
 AFRAME.registerComponent('mobile', {
   init: function () {
     if (AFRAME.utils.device.isMobile()) {
-      console.log('MOBILE')
       document.querySelector('.phone').setAttribute('visible', true)
     } else {
-      console.log('PC')
       document.querySelector('.phone').setAttribute('visible', true)
     // document.querySelector('.phone').remove()
     }
 
     var teleport_pads = document.querySelectorAll('#teleport')
-    this.onBackgroundClick = this.teleport.bind(this)
+    this.teleport = this.teleport.bind(this)
     for (var i = 0; i < teleport_pads.length; ++i) {
       teleport_pads[i].addEventListener('click', this.teleport)
     }
