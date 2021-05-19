e0158f63176864aae759b249047a82d6601e5cd4
Argett <lilian.favre.garcia@efrei.net>
2021-05-19 14:29:03 +0200

    Creation of empty teleport function

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -6,7 +6,19 @@ AFRAME.registerComponent('mobile', {
       document.querySelector('.phone').setAttribute('visible', true)
     } else {
       console.log('PC')
-      document.querySelector('.phone').remove()
+    // document.querySelector('.phone').remove()
     }
+
+    var teleport_pads = document.querySelectorAll('#teleport')
+    this.onBackgroundClick = this.teleport.bind(this)
+    for (var i = 0; i < teleport_pads.length; ++i) {
+      teleport_pads[i].addEventListener('click', this.teleport)
+    }
+
+  },
+
+  teleport: function (evt) {
+    //document.querySelector('#camera').setAttribute('position', )
+    console.log(this)
   }
 })
