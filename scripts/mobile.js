e2507f14a6c6fa279cf025357575b66a5e99bbb9
Argett <lilian.favre.garcia@efrei.net>
2021-05-19 15:44:07 +0200

    Make all pads visible

--- scripts/mobile.js
+++ scripts/mobile.js
@@ -2,14 +2,14 @@
 AFRAME.registerComponent('mobile', {
   init: function () {
     if (AFRAME.utils.device.isMobile()) {
-      document.querySelector('.phone').setAttribute('visible', true)
+      document.querySelectorAll('.phone').setAttribute('visible', true)
     } else {
-      document.querySelector('.phone').setAttribute('visible', true)
+      console.log()
+      document.querySelectorAll('.phone').setAttribute('visible', true)
     // document.querySelector('.phone').remove()
     }
 
     var teleport_pads = document.querySelectorAll('#teleport')
-    //this.teleport = this.teleport.bind(this)
     for (var i = 0; i < teleport_pads.length; ++i) {
       teleport_pads[i].addEventListener('click', this.teleport)
     }
